433f3fe5c3216418021b4139f7dad7cbb82ec928
Juan Garcia <juagarca@gmail.com>
2021-01-30 14:36:14 +0000

    added event listener to search input, a couple of color changes and fixed title on home screen to the top

--- public/application.js
+++ public/application.js
@@ -1,1 +1,8 @@
-console.log("aki");
+const input = document.getElementById('search-input');
+
+// if in search page add key up event to the search input
+if (document.getElementById('search-input')) {
+  input.addEventListener('keyup', (event) => {
+    const query = event.currentTarget.value;
+  });
+}
